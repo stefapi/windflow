a2e681ccdc4fd87f9f695003f9a2242a94fe7676
stephane apiou <stephane@apiou.org>
2026-03-30 04:07:19 +0200

    chore: cleanup colibri project

--- colibri/src/routes/images/+page.server.ts
+++
@@ -1,9 +0,0 @@
-import type { PageServerLoad } from './$types';
-import { getScannerSettings } from '$lib/server/scanner';
-
-export const load: PageServerLoad = async () => {
-	const { scanner } = await getScannerSettings();
-	return {
-		scannerEnabled: scanner !== 'none'
-	};
-};
